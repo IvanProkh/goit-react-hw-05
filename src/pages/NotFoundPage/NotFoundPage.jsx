da72836090531630e096a8e2ca4ca832fd49380a
IvanProkhorets <prohorets.ivan@gmail.com>
2024-04-07 01:05:01 +0300

    update 1.0

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import css from './NotFoundPage.module.css';
 
 export default function NotFoundPage() {
@@ -5,6 +6,9 @@ export default function NotFoundPage() {
     <div className={css.div}>
       <h1 className={css.text}>Oops!</h1>
       <h2 className={css.text}>Something went wrong...</h2>
+      <Link className={css.textLink} to="/">
+        <h2>Back to home page!</h2>
+      </Link>
     </div>
   );
 }
